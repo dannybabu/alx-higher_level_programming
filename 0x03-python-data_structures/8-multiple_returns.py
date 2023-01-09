04298e1e34ece4d4ba8cd3af2a5802c4577b3510
dannybabu <danny1babu2@gmail.com>
2023-01-09 07:58:56 -0800

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        my_tuple = (0, none)
+    else:
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
